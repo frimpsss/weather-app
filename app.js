029083d1c2c13fb36bf999ac97cf0552be0376c0
Akwasi Frimpong <84427404+frimpsss@users.noreply.github.com>
2022-07-08 21:44:57 +0000

    [fix] - loader for weather icon

--- app.js
+++ app.js
@@ -122,7 +122,31 @@ async function getWeatherData(city){
             const{name, main: {temp, pressure, humidity}, wind: {speed, deg}, weather, sys: {country}, coord: {lon, lat}} = list[0]
             const{id, description, icon} = weather[0]
             // console.log(name, temp, pressure, humidity, speed, deg, id, description, icon)
-            const iconImage = `http://openweathermap.org/img/wn/${icon}@2x.png`
+            if(!icon){
+                const iconImage = `
+                <svg version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve">
+                <rect x="20" y="50" width="4" height="10" fill="#fff">
+                      <animateTransform attributeType="xml"
+                      attributeName="transform" type="translate"
+                        values="0 0; 0 20; 0 0"
+                        begin="0" dur="0.6s" repeatCount="indefinite" />
+                        </rect>
+                    <rect x="30" y="50" width="4" height="10" fill="#fff">
+                      <animateTransform attributeType="xml"
+                        attributeName="transform" type="translate"
+                        values="0 0; 0 20; 0 0"
+                        begin="0.2s" dur="0.6s" repeatCount="indefinite" />
+                    </rect>
+                    <rect x="40" y="50" width="4" height="10" fill="#fff">
+                    <animateTransform attributeType="xml"
+                        attributeName="transform" type="translate"
+                        values="0 0; 0 20; 0 0"
+                        begin="0.4s" dur="0.6s" repeatCount="indefinite" />
+                        </rect>
+                        </svg>`
+            }
+            else {const iconImage = `http://openweathermap.org/img/wn/${icon}@2x.png`}
             //  seting image
             const img = document.querySelector('.icon img')
             img.src = iconImage
